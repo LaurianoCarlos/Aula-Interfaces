e3cfd458af4600937997f550e4f5e6d1b5cb043c
LAURIANO CARLOS ALVES BATISTA <lauriano.cabatista@senacsp.edu.br>
2024-03-01 22:20:30 -0300

    Metodo Singletin

--- PadroesDeProjeto/Singleton/src/cliente/UsaSingleton.java
+++ PadroesDeProjeto/Singleton/src/cliente/UsaSingleton.java
@@ -1,5 +1,24 @@
 package cliente;
 
-public class UsaSingleton {
+import singleton.Singleton;
 
+public class UsaSingleton extends Thread {
+
+    public void run(){
+        Singleton s1 = Singleton.getInstance();
+        System.out.println(s1);
+    }
+    public static void main(String[] args) {
+
+        UsaSingleton t1 = new UsaSingleton();
+        UsaSingleton t2 = new UsaSingleton();
+        UsaSingleton t3 = new UsaSingleton();
+        UsaSingleton t4 = new UsaSingleton();
+
+        t1.start();
+        t2.start();
+        t3.start();
+        t4.start();
+
+    }
 }
--- PadroesDeProjeto/Singleton/src/singleton/Singleton.java
+++ PadroesDeProjeto/Singleton/src/singleton/Singleton.java
@@ -1,11 +1,15 @@
 package singleton;
 
 public  class Singleton {
-    private static Singleton singleton;
+    private static Singleton singleton = new Singleton();
 
     private Singleton(){
 
     }
 
+    /**
+     * Método global de acesso a variavel da classe Singleton
+     * @return Singleton
+     */
     public static Singleton getInstance(){return singleton;}
 }
